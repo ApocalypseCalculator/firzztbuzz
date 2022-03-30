d4802df098d90ea8ba104b9922facc11fdd372f1
Patrick Lin <apocalypsecalculator@gmail.com>
2022-03-30 10:48:40 -0400

    buzz avail

--- websocket/index.ts
+++ websocket/index.ts
@@ -2,10 +2,12 @@ import joinRoom from "./joinRoom";
 import createRoom from "./createRoom";
 import disconnect from "./disconnect";
 import setBuzzer from "./setBuzzer";
+import buzz from "./buzz";
 
 export default [
     joinRoom,
     createRoom,
     disconnect,
-    setBuzzer
+    setBuzzer,
+    buzz
 ]
