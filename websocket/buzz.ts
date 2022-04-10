22bd2371aefebdfed5b9e1d8b069bb9fcc106f01
Patrick Lin <apocalypsecalculator@gmail.com>
2022-04-09 20:15:52 -0400

    fix buzz

--- websocket/buzz.ts
+++ websocket/buzz.ts
@@ -2,12 +2,15 @@ import { WebSocketFunction } from "../types/index";
 import { User } from "../types";
 
 export default new WebSocketFunction({
-    id: "setBuzzer",
+    id: "buzz",
     call: (content, socket, server) => {
         let user = content.authUser as User;
         let room = server.roomMap.get(user.room);
         if (!room!.buzzer.started) {
             socket.emit('error', 'Buzzer has not been started or has ended already');
+        }
+        else if (room!.buzzer.textrequired && !content.text) {
+            socket.emit('error', 'Text is required for this submission');
         }
         else {
             let timetaken = Date.now() - room!.buzzer.time;
